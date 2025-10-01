be84bb0bacbc27568847a0ca4fcb3a38d06f23b0
Oliwia Lesniak <oliwialesniak666@gmail.com>
2025-10-01 11:02:25 +0200

    Page object

--- src/pageObject.js
+++ src/pageObject.js
@@ -1,17 +1,45 @@
 'use strict';
 
+// Header class
 class Header {
+  get logo() {
+    return 'Conduit logo';
+  }
+}
 
-};
-
+// PageObject class
 class PageObject {
+  constructor(url) {
+    this.url = url;
+    this.header = new Header();
+  }
 
-};
+  clickOnLogo() {
+    return 'Click on the ' + this.header.logo;
+  }
+}
 
+// ArticlePage class extending PageObject
 class ArticlePage extends PageObject {
+  constructor(url, slug) {
+    super(url);
+    this.url = url + slug; // concatenate url and slug
+  }
 
-};
+  get commentButton() {
+    return '[Publish comment] button';
+  }
 
+  clickOnCommentButton() {
+    return 'Click on the ' + this.commentButton;
+  }
+
+  assertPageOpened() {
+    return `The ${this.url} is opened`;
+  }
+}
+
+// Export classes for testing
 module.exports = {
   Header,
   PageObject,
